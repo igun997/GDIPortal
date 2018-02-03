5ae5246ea52696e05f7882596261b89322e9712a
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-03 15:37:50 +0700

    RE-Configure Autoload & Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,14 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples: my-controller/index -> my_controller/index
 |   my-controller/my-method -> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
+$route['default_controller'] = 'init';
+$route['404_override'] = 'error404';
 $route['translate_uri_dashes'] = TRUE;
-
-/*
-| -------------------------------------------------------------------------
-| Sample REST API Routes
-| -------------------------------------------------------------------------
-*/
-$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
-$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
+$route['public'] = "public/home";
+$route['gdiadmin'] = "admin/home";
