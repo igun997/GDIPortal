49920cc027ca2e84dc8f95b31ac202c204bc4938
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-04 20:24:19 +0700

    Completely Detail Page

--- application/controllers/public/Detail.php
+++ application/controllers/public/Detail.php
@@ -6,13 +6,27 @@ class Detail extends CI_Controller{
   public function __construct()
   {
     parent::__construct();
-    //Codeigniter : Write Less Do More
+    $this->load->model("donatur");
+    $this->load->model("event");
   }
 
   function page($slug = "")
   {
     if($slug != ""){
-      $this->template->setTemplate("public/deskripsi",array("judul"=>"Contoh"),"themepublic/header","themepublic/footer");
+      $resCSS = $this->config->item("css");
+      $resJS = $this->config->item("js");
+      //Tambah Custom JS
+      array_push($resJS,base_url("assets/js/detail.js"));
+      //Get Detail Event
+      $eventdetail = $this->event->find(array("slug"=>$slug));
+      if($eventdetail->num_rows() > 0){
+        //Get Event Selanjutnya
+        $nowdate = date("Y-m-d H:i:s");
+        $eventselanjutnya = $this->event->find(array("mulai_acara >="=>$nowdate));
+        $this->template->setTemplate("public/deskripsi",array("judul"=>$eventdetail->row()->judul,"event_upcoming"=>$eventselanjutnya->result(),"slug"=>$slug,"event"=>$eventdetail->row()),"themepublic/header","themepublic/footer",$resCSS,$resJS);
+      }else{
+        redirect(base_url("notfound"));
+      }
     }else{
       redirect(base_url("notfound"));
     }
