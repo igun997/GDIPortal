c01c8669bf2bc133432b5704dec45bd2ae2edc8c
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-04 20:28:09 +0700

    Add Independent Twitter API

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+
+    // Consumer Key
+    define('CONSUMER_KEY', 'zcn3jbLYnw3n3tbJ8xJ71G5zX');
+    define('CONSUMER_SECRET', 'XnWgkU2DkPgXwXSs5W0itHHGp87nbuYrfuh4VTUOKaB2KOQ7xy');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '614238672-6eOyOPxVc7fmStNLe5eWjOMIalg0Tin1hP4pzr85');
+    define('ACCESS_SECRET', 'fnB292xXT7zQDI2FaJs60rhkmCjWhYsNNqgttgCwnhJdH');
