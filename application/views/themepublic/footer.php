5a2f98252cdfff36f98cd9fb02f08c9b48156696
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-04 20:26:30 +0700

    Change Footer Structure And Remove Placeholder

--- application/views/themepublic/footer.php
+++ application/views/themepublic/footer.php
@@ -1,37 +1,34 @@
 </div>
 <!-- Site Footer -->
-<div class="site-footer parallax parallax3" style="background-image:url(http://placehold.it/1280x800&amp;text=IMAGE+PLACEHOLDER)">
+<div class="site-footer parallax parallax3" style="background-image:url(<?= base_url("assets/patterns/pt14.jpg") ?>)">
   <div class="container">
       <div class="row">
           <div class="col-md-4 col-sm-4">
               <div class="widget footer_widget">
-                  <h4 class="widgettitle">About Born to Give</h4>
+                  <h4 class="widgettitle">Tentang YGDI</h4>
                     <p><img src="images/logo.png" alt=""></p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
+                    <p>Kami adalah Yayasan Non Profit dengan visi menjadi lembaga profesional bertaraf internasional yang membangun masyarakat produktif, mandiri, dan mulia. Kami membutuhkan bantuan anda untuk kegiatan dan pembinaan Dhuafa di Indonesia</p>
                     <ul class="social-icons-rounded social-icons-colored">
-                        <li class="facebook"><a href="#"><i class="fa fa-facebook-f"></i></a></li>
-                        <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li class="googleplus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li class="youtube"><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                        <li class="vimeo"><a href="#"><i class="fa fa-vimeo"></i></a></li>
-                        <li class="instagram"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li class="facebook"><a href="<?= $this->config->item("fb") ?>"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="twitter"><a href="<?= $this->config->item("twitter") ?>"><i class="fa fa-twitter"></i></a></li>
+                        <li class="googleplus"><a href="<?= $this->config->item("google") ?>"><i class="fa fa-google-plus"></i></a></li>
+                        <li class="youtube"><a href="<?= $this->config->item("youtube") ?>"><i class="fa fa-youtube-play"></i></a></li>
+                        <li class="vimeo"><a href="<?= $this->config->item("vimeo") ?>"><i class="fa fa-vimeo"></i></a></li>
+                        <li class="instagram"><a href="<?= $this->config->item("instagram") ?>"><i class="fa fa-instagram"></i></a></li>
                     </ul>
                 </div>
             </div>
           <div class="col-md-4 col-sm-4">
               <div class="widget footer_widget widget_links">
-                  <h4 class="widgettitle">Blogroll</h4>
+                  <h4 class="widgettitle">Link Tertaut</h4>
                     <ul>
-                      <li><a href="#">Become a volunteer</a></li>
-                      <li><a href="#">Our mission</a></li>
-                      <li><a href="#">Success stories</a></li>
-                      <li><a href="#">Meet our team</a></li>
+                      <li><a href="#">Basis Data</a></li>
                     </ul>
                 </div>
             </div>
           <div class="col-md-4 col-sm-4">
               <div class="widget footer_widget">
-                  <h4 class="widgettitle">We are on Twitter</h4>
+                  <h4 class="widgettitle">Lihat Kami Di Twitter</h4>
                     <div class="twitter-widget" data-tweets-count="2"></div>
                 </div>
             </div>
@@ -44,7 +41,7 @@
       <div class="row">
           <div class="col-md-6 col-sm-6">
               <div class="copyrights-col-left">
-                  <p>&copy; 2016 Born to Give. All Rights Reserved</p>
+                  <p>&copy; 2018 Graha Dhuafa Indonesia. All Rights Reserved</p>
                 </div>
             </div>
           <div class="col-md-6 col-sm-6"></div>
