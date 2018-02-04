45323b28e44a8d6b879dbf920e40639535f42d01
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-04 20:25:55 +0700

    Remove Placeholder, Completely View of Detail

--- application/views/public/deskripsi.php
+++ application/views/public/deskripsi.php
@@ -1,76 +1,66 @@
 <div class="content">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 content-block">
-                <h3>Help rebuild Nepal</h3>
+            <div id="konten" class="col-md-8 content-block">
+                <h3><?= $data["judul"] ?></h3>
                 <div class="post-media">
-                    <img src="http://placehold.it/900x600&amp;text=IMAGE+PLACEHOLDER" alt="">
-                  </div>
+                    <img src="<?= base_url("_upload/".$data["event"]->gambar) ?>" alt="">
+                </div>
+                <?php
+                  //Detail Data
+                  $dataEvent = $this->event->eventdonasi(array("slug"=>$data["slug"]));
+                  $total = 0;
+                  foreach ($dataEvent->result() as $key => $value) {
+                    $total = $total +  $value->total_donasi;
+                  }
+                  $persentasi = ($total*100)/$data["event"]->target_biaya;
+                  $diffdays = $this->template->diffdays(date("Y-m-d H:i:s"),$data["event"]->selesai_donasi);
+                  $diffdaysstart = $this->template->diffdays(date("Y-m-d H:i:s"),$data["event"]->mulai_donasi);
+                ?>
                   <span class="label label-default">Dana Terkumpul</span>
                   <div class="progress">
-                      <div style="width: 88%;" class="progress-bar progress-bar-primary" data-appear-progress-animation="50%" data-appear-animation-delay="100"> <span class="progress-bar-tooltip">50%</span> </div>
+                        <div style="width: 88%;" class="progress-bar progress-bar-primary" data-appear-progress-animation="<?= $persentasi ?>%" data-appear-animation-delay="100"> <span class="progress-bar-tooltip"><?= $persentasi ?>%</span> </div>
                   </div>
-                  <div class="pull-left">Terkumpul <strong>Rp.1.000.000</strong></div>
-                  <div class="pull-right">Target <strong class="accent-color">Rp. 2.000.000</strong></div>
+                  <div class="pull-left">Terkumpul <strong>Rp.<?= number_format($total) ?></strong></div>
+                  <div class="pull-right">Target <strong class="accent-color">Rp. <?= number_format($data["event"]->target_biaya) ?></strong></div>
                   <div class="spacer-20"></div>
                   <div class="row">
                     <div class="col-md-5 col-sm-5">
-                      <p class="lead">Nepal has been seriously devastated by the recent earthquake. Over 8,000 people have died (with death toll rising), thousands more are injured, and countless more have been displaced. Many homes, temples, and public monuments have been destroyed throughout the affected areas.</p>
+                      <p class="lead"><?= $data["event"]->rangkuman ?></p>
                     </div>
                       <div class="col-md-7 col-sm-7">
                           <ul class="list-group">
                           <li class="list-group-item">Total Donatur<span class="badge">2</span></li>
-                          <li class="list-group-item">Waktu Donasi Berakhir<span class="badge">10 Hari</span></li>
+                          <li class="list-group-item">Waktu Donasi Dimulai <span class="badge"><?= ($diffdaysstart > 0)?$diffdaysstart." Hari Lagi":"Sudah Dimulai" ?></span></li>
+                          <li class="list-group-item">Waktu Donasi Berakhir <span class="badge"><?= ($diffdays > 0)?$diffdays." Hari Lagi":"Donasi Ditutup" ?> </span></li>
                         </ul>
                           <button type="button" class="btn btn-primary btn-block" id="donasi" data-id="{id}">Saya Akan Bantu</button>
                       </div>
                   </div>
-                  <p>Vestibulum quam nisi, pretium a nibh sit amet, consectetur hendrerit mi. Aenean imperdiet lacus sit amet elit porta, et malesuada erat bibendum. Cras sed nunc massa. Quisque tempor dolor sit amet tellus malesuada, malesuada iaculis eros dignissim. Aenean vitae diam id lacus fringilla maximus. Mauris auctor efficitur nisl, non blandit urna fermentum nec.</p>
-                  <img src="http://placehold.it/320x200&amp;text=IMAGE+PLACEHOLDER" alt="" class="align-left">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa ipsum, efficitur a fermen tum sed, suscipit sit amet arcu. Ut ut finibus tortor, eu ultrices turpis. Mauris vitae elit nec diam elementum elementum. Mauris ante quam, consequat ac nibh placerat, lacinia sollicitudin mi. Duis facilisis nibh quam, sit amet interdum tellus sollicitudin tempor. Curabitur aliquam erat in nisl lobortis, ut pellentesque lectus viverra.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa ipsum, efficitur a fermen tum sed, suscipit sit amet arcu. Ut ut finibus tortor, eu ultrices turpis. Mauris vitae elit nec diam elementum elementum. Mauris ante quam, consequat ac nibh placerat, lacinia sollicitudin mi. Duis facilisis nibh quam, sit amet interdum tellus sollicitudin tempor. Curabitur aliquam erat in nisl lobortis, ut pellentesque lectus viverra. Aenean sodales aliquet arcu at aliquam. Vestibulum quam nisi, pretium a nibh sit amet, consectetur hendrerit mi. Aenean imperdiet lacus sit amet elit porta, et malesuada erat bibendum. Cras sed nunc massa. Quisque tempor dolor sit amet tellus malesuada, malesuada iaculis eros dignissim. Aenean vitae diam id lacus fringilla maximus. Mauris auctor efficitur nisl, non blandit urna fermentum nec.</p>
+                  <?= $data["event"]->deskripsi ?>
               </div>
 
               <!-- Sidebar -->
               <div class="col-md-4 sidebar-block">
                   <!-- Latest Events -->
                 <div class="widget events-calendar-widget">
-                      <h3 class="widgettitle">Upcoming Events</h3>
+                      <h3 class="widgettitle">Acara Yang Akan Datang</h3>
                       <div class="events-calendar-widget-body">
                           <ul class="events-compact-list">
-                            <li class="event-list-item">
+                            <?php foreach ($data["event_upcoming"] as $key => $value): ?>
+                              <li class="event-list-item">
                                 <span class="event-date">
-                                    <span class="date">14</span>
-                                      <span class="month">Jan</span>
-                                      <span class="year">2016</span>
+                                    <span class="date"><?= date("d",strtotime($value->mulai_acara)) ?></span>
+                                      <span class="month"><?= date("M",strtotime($value->mulai_acara)) ?></span>
+                                      <span class="year"><?= date("Y",strtotime($value->mulai_acara)) ?></span>
                                   </span>
                                   <div class="event-list-cont">
-                                      <span class="meta-data">Thursday, 11:20 AM</span>
-                                    <h4 class="post-title"><a href="#">Summer Camp: Students Get Together</a></h4>
+                                    <span class="meta-data"><?= date("D",strtotime($value->mulai_acara)) ?>, <?= date("H:i:s",strtotime($value->mulai_acara)) ?></span>
+                                    <h4 class="post-title"><a href="#"><?= $value->judul ?></a></h4>
+                                    <p><?= $value->deskripsi ?></p>
                                   </div>
                               </li>
-                            <li class="event-list-item">
-                                <span class="event-date">
-                                    <span class="date">18</span>
-                                      <span class="month">Jan</span>
-                                      <span class="year">2016</span>
-                                  </span>
-                                  <div class="event-list-cont">
-                                      <span class="meta-data">Monday, 07:00 PM</span>
-                                    <h4 class="post-title"><a href="#">Fundraising for meals</a></h4>
-                                  </div>
-                              </li>
-                            <li class="event-list-item">
-                                <span class="event-date">
-                                    <span class="date">26</span>
-                                      <span class="month">Feb</span>
-                                      <span class="year">2016</span>
-                                  </span>
-                                  <div class="event-list-cont">
-                                      <span class="meta-data">Friday, 01:00 PM</span>
-                                    <h4 class="post-title"><a href="#">Green Environment</a></h4>
-                                  </div>
-                              </li>
+                            <?php endforeach; ?>
                           </ul>
                       </div>
                   </div>
