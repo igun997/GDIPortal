7b6587b116d9e6e125a012a263252eef8136201a
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-04 20:27:18 +0700

    RE-Arrange

--- assets/js/helper-plugins.js
+++ assets/js/helper-plugins.js
@@ -253,7 +253,7 @@
 /*
  * jQuery throttle / debounce - v1.1 - 3/7/2010
  * http://benalman.com/projects/jquery-throttle-debounce-plugin/
- * 
+ *
  * Copyright (c) 2010 "Cowboy" Ben Alman
  * Dual licensed under the MIT and GPL licenses.
  * http://benalman.com/about/license/
@@ -282,7 +282,7 @@ if(p&&p.src===r){d(q,p.isBroken);return}if(q.complete&&q.naturalWidth!==b){d(q,q
 
 /*
  * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
- 
+
 */
 
 // t: current time, b: begInnIng value, c: change In value, d: duration
@@ -397,7 +397,7 @@ jQuery.extend( jQuery.easing,
 		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
 	},
 	easeInOutBack: function (x, t, b, c, d, s) {
-		if (s == undefined) s = 1.70158; 
+		if (s == undefined) s = 1.70158;
 		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
 		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
 	},
@@ -750,15 +750,15 @@ http://www.gnu.org/licenses/gpl.html
 		var getHeight;
 		var firstTop;
 		var paddingTop = 0;
-		
-		//get the starting position of each element to have parallax applied to it	
+
+		//get the starting position of each element to have parallax applied to it
 		function update (){
-			
+
 			$this.each(function(){
-								
+
 				firstTop = $this.offset().top;
 			});
-	
+
 			if (outerHeight) {
 				getHeight = function(jqo) {
 					return jqo.outerHeight(true);
@@ -768,30 +768,30 @@ http://www.gnu.org/licenses/gpl.html
 					return jqo.height();
 				};
 			}
-				
+
 			// setup defaults if arguments aren't specified
 			if (arguments.length < 1 || xpos === null) xpos = "50%";
 			if (arguments.length < 2 || speedFactor === null) speedFactor = 0.5;
 			if (arguments.length < 3 || outerHeight === null) outerHeight = true;
-			
+
 			// function to be called whenever the window is scrolled or resized
-			
-				var pos = $window.scrollTop();				
-	
+
+				var pos = $window.scrollTop();
+
 				$this.each(function(){
 					var $element = $(this);
 					var top = $element.offset().top;
 					var height = getHeight($element);
-	
+
 					// Check if totally above or totally below viewport
 					if (top + height < pos || top > pos + windowHeight) {
 						return;
 					}
-					
+
 					$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
-					
+
 				});
-		}		
+		}
 
 		$window.bind('scroll', update).resize(update);
 		update();
@@ -830,7 +830,7 @@ b=n(b);var l=q(a,b,c,g),k=d.transit.enabled&&e.transition?parseInt(b,10)+parseIn
  * Thanks to: Seamus Leahy for adding deltaX and deltaY
  *
  * Version: 3.0.6
- * 
+ *
  * Requires: 1.2.2+
  */
 (function(a){function d(b){var c=b||window.event,d=[].slice.call(arguments,1),e=0,f=!0,g=0,h=0;return b=a.event.fix(c),b.type="mousewheel",c.wheelDelta&&(e=c.wheelDelta/120),c.detail&&(e=-c.detail/3),h=e,c.axis!==undefined&&c.axis===c.HORIZONTAL_AXIS&&(h=0,g=-1*e),c.wheelDeltaY!==undefined&&(h=c.wheelDeltaY/120),c.wheelDeltaX!==undefined&&(g=-1*c.wheelDeltaX/120),d.unshift(b,e,g,h),(a.event.dispatch||a.event.handle).apply(this,d)}var b=["DOMMouseScroll","mousewheel"];if(a.event.fixHooks)for(var c=b.length;c;)a.event.fixHooks[b[--c]]=a.event.mouseHooks;a.event.special.mousewheel={setup:function(){if(this.addEventListener)for(var a=b.length;a;)this.addEventListener(b[--a],d,!1);else this.onmousewheel=d},teardown:function(){if(this.removeEventListener)for(var a=b.length;a;)this.removeEventListener(b[--a],d,!1);else this.onmousewheel=null}},a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})})(jQuery);
