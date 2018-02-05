082a78ab3b211f45f6c27b96174c2ec7ded0b217
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-05 09:42:20 +0700

    Login Scripts

--- assets/backend/js/custom-script.js
+++ assets/backend/js/custom-script.js
@@ -9,3 +9,37 @@ NOTE:
 ------
 PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+$("#submitLogin").on("click", function() {
+  var dataLogin = $("#formLogin").serializeArray();
+  console.log("Login Exc");
+  console.log(dataLogin);
+  swal({
+    title: 'Sedang Di Proses',
+    text: 'Harap Tunggu',
+    timer: 2000,
+    onOpen: () => {
+      swal.showLoading()
+    }
+  }).then((result) => {
+    if (result.dismiss === 'timer') {
+      $.ajax({
+          url: base_url+"/data/login",
+          type: 'POST',
+          dataType: 'json',
+          data: dataLogin
+        })
+        .done(function(a) {
+          if(a.status){
+            swal("Login Berhasil",a.msg,"success").then((result)=>{
+              location.href = base_url+"/pengguna/home";
+            });
+          }else{
+            swal("Login Gagal",a.msg,"error")
+          }
+        })
+        .fail(function() {
+          swal("Login Gagal","Cek Koneksi Jaringan Kamu","error")
+        })
+    }
+  })
+});
