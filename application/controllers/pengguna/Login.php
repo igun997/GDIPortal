06f813ce110ca4af0ddd2f37501088d5c5e119a6
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-05 09:42:05 +0700

    Add Sweat Alert

--- application/controllers/pengguna/Login.php
+++ application/controllers/pengguna/Login.php
@@ -17,12 +17,14 @@ class Login extends CI_Controller{
       base_url("assets/backend/").'css/style.min.css',
       base_url("assets/backend/").'css/custom/custom-style.css',
       base_url("assets/backend/").'css/layouts/page-center.css',
+      base_url("assets/vendor/sweetalert/sweetalert2.css"),
       base_url("assets/backend/").'js/plugins/prism/prism.css',
       base_url("assets/backend/").'js/plugins/perfect-scrollbar/perfect-scrollbar.css'
     );
     $js = array(
       base_url("assets/backend/").'js/plugins/jquery-1.11.2.min.js',
       base_url("assets/backend/").'js/materialize.min.js',
+      base_url("assets/vendor/sweetalert/sweetalert2.js"),
       base_url("assets/backend/").'js/plugins/prism/prism.js',
       base_url("assets/backend/").'js/plugins/perfect-scrollbar/perfect-scrollbar.min.js',
       base_url("assets/backend/").'js/plugins.min.js',
