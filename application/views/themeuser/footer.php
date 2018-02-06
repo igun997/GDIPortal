0f2fc8cf0de1073d543fbef08e70460b65e99c53
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-06 12:20:01 +0700

    Remove Un-used + Rearrange Scripts

--- application/views/themeuser/footer.php
+++ application/views/themeuser/footer.php
@@ -1,25 +1,9 @@
 <!-- START FOOTER -->
-<footer class="page-footer">
-    <div class="container">
-        <div class="row section">
-            <div class="col l6 s12">
-                <h5 class="white-text">World Market</h5>
-                <p class="grey-text text-lighten-4">World map, world regions, countries and cities.</p>
-                <div id="world-map-markers"></div>
-            </div>
-            <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Sales by Country</h5>
-                <p class="grey-text text-lighten-4">A sample polar chart to show sales by country.</p>
-                <div id="polar-chart-holder">
-                    <canvas id="polar-chart-country" width="200"></canvas>
-                </div>
-            </div>
-        </div>
-    </div>
+<footer class="page-footer" style="position: fixed;bottom: 0;width:100%">
     <div class="footer-copyright">
         <div class="container">
-            Copyright © 2015 <a class="grey-text text-lighten-4" href="http://themeforest.net/user/geekslabs/portfolio?ref=geekslabs" target="_blank">GeeksLabs</a> All rights reserved.
-            <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="http://geekslabs.com/">GeeksLabs</a></span>
+            Copyright © 2018 <a class="grey-text text-lighten-4" href="http://themeforest.net/user/geekslabs/portfolio?ref=geekslabs" target="_blank">Graha Dhuafa Indonesia</a> All rights reserved.
+            <span class="right"> Design and Developed by SystemFive</span>
         </div>
     </div>
 </footer>
@@ -29,56 +13,24 @@
 <!-- ================================================
 Scripts
 ================================================ -->
-
-<!-- jQuery Library -->
-<script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>
-<!--materialize js-->
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<!--scrollbar-->
-<script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-
-
-<!-- chartist -->
-<script type="text/javascript" src="js/plugins/chartist-js/chartist.min.js"></script>
-
-<!-- chartjs -->
-<script type="text/javascript" src="js/plugins/chartjs/chart.min.js"></script>
-<script type="text/javascript" src="js/plugins/chartjs/chart-script.js"></script>
-
-<!-- sparkline -->
-<script type="text/javascript" src="js/plugins/sparkline/jquery.sparkline.min.js"></script>
-<script type="text/javascript" src="js/plugins/sparkline/sparkline-script.js"></script>
-
-<!-- google map api -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAZnaZBXLqNBRXjd-82km_NO7GUItyKek"></script>
-
-<!--jvectormap-->
-<script type="text/javascript" src="js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script type="text/javascript" src="js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script type="text/javascript" src="js/plugins/jvectormap/vectormap-script.js"></script>
-
-<!--google map-->
-<script type="text/javascript" src="js/plugins/google-map/google-map-script.js"></script>
-
-
-<!--plugins.js - Some Specific JS codes for Plugin Settings-->
-<script type="text/javascript" src="js/plugins.min.js"></script>
-<!--custom-script.js - Add your own theme custom JS-->
-<script type="text/javascript" src="js/custom-script.js"></script>
-<!-- Toast Notification -->
+<?php
+  foreach ($js as $key => $value) {
+      printf("<script src='%s'></script>\n",$value);;
+  }
+?>
 <script type="text/javascript">
 // Toast Notification
-$(window).load(function() {
-    setTimeout(function() {
-        Materialize.toast('<span>Hiya! I am a toast.</span>', 1500);
-    }, 1500);
-    setTimeout(function() {
-        Materialize.toast('<span>You can swipe me too!</span>', 3000);
-    }, 5000);
-    setTimeout(function() {
-        Materialize.toast('<span>You have new order.</span><a class="btn-flat yellow-text" href="#">Read<a>', 3000);
-    }, 15000);
-});
+// $(window).load(function() {
+//     setTimeout(function() {
+//         Materialize.toast('<span>Hiya! I am a toast.</span>', 1500);
+//     }, 1500);
+//     setTimeout(function() {
+//         Materialize.toast('<span>You can swipe me too!</span>', 3000);
+//     }, 5000);
+//     setTimeout(function() {
+//         Materialize.toast('<span>You have new order.</span><a class="btn-flat yellow-text" href="#">Read<a>', 3000);
+//     }, 15000);
+// });
 </script>
 </body>
 
