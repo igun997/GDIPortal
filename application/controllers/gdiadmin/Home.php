62fa6072ee55b850a3af65c59defe99237d5ecf2
Indra Gunanda <indra.gunanda@gmail.com>
2018-02-09 16:51:32 +0700

    Restrict Login Admin

--- application/controllers/gdiadmin/Home.php
+++ application/controllers/gdiadmin/Home.php
@@ -6,13 +6,16 @@ class Home extends CI_Controller{
   public function __construct()
   {
     parent::__construct();
-    //Codeigniter : Write Less Do More
+    if($this->session->adminLogin == null){
+      redirect(base_url("gdiadmin/login"));
+    }
   }
 
   function index()
   {
     $css = $this->config->item("cssadmin");
     $js = $this->config->item("jsadmin");
+    array_push($js,base_url("assets/backend/js/dashboard.js"));
     //Render Page
     $this->template->setTemplate("admin/home",array("judul"=>"Halaman Admin"),"themeadmin/header","themeadmin/footer",$css,$js);
   }
